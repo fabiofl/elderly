72ff20059a4d6e1b8da8e8871332942b3d9e0c48
patymori <excermori@yahoo.com.br>
2017-03-12 13:21:56 -0300

    Event detail

--- app/components/event/event.controller.js
+++ app/components/event/event.controller.js
@@ -1,25 +1,48 @@
 (function(){
 	angular.module('elderly.event', [])
 
-	.controller('EventController', ['$scope', '$firebaseArray', '$state',
-		function($scope, $firebaseArray, $state){
+	.controller('EventController', ['$scope', '$firebaseArray', '$firebaseObject', '$state',
+		function($scope, $firebaseArray, $firebaseObject, $state){
 			var ref = firebase.database().ref('events');
 			var events = $firebaseArray(ref);
 
 	    	$scope.events = events;
+            $scope.event = null;
 			$scope.newEvent = {};
 			$scope.isOpen = false;
+
+            if($state.is('saveEvent')) {
+                var eventRef = firebase.database().ref('events/'+$state.params.id);
+                eventRef.on('value', function(snap) {
+                    $scope.event = snap.val()
+                })
+            }
 
 			$scope.eventNew = function() {
 				$state.go('addEvent');
 			}
 
+            $scope.eventRead = function(event) {
+                $scope.event = event;
+				$state.go('saveEvent', {
+                    id: event.$id
+                });
+			}
+
 	    	$scope.addEvent = function() {
-	    		
+
 	    		ref.push().set($scope.newEvent).then(function(){
 	    			$state.go('events');
 	    		});
-	    		
+
+	    	}
+
+            $scope.saveEvent = function() {
+
+	    		ref.push().set($scope.event).then(function(){
+	    			$state.go('events');
+	    		});
+
 	    	}
 	}]);
 
