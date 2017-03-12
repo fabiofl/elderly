4997907993ee744554d3695175fc1c265a66d11c
Fabio Fernandes <fabio@mosh.io>
2017-03-12 02:20:25 -0300

    Add Angular Material

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,14 +1,17 @@
 (function(){
 	angular.module('elderly.routes', ['ui.router'])
 
-	.config(function($stateProvider, $urlRouterProvider){
+	.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 		$urlRouterProvider.otherwise('/home');
 
 		$stateProvider
-		.state('home', {
-			url: '/home',
-			templateUrl: 'components/home/home.view.html',
-			controller: 'HomeController'
-		});
+			.state('home', {
+				url: '/home',
+				templateUrl: 'components/home/home.view.html',
+				controller: 'HomeController'
+			});
+			
+		$locationProvider.html5Mode(true);
+
 	});
 })();
